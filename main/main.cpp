bcb50d58306c96f024d5524513613dc06c6d067a
SergeyZagrebin <sergey.127.0.0.1@ya.ru>
2022-04-28 17:10:53 +0300

    Tree

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "Tree.h"
+using namespace std;
 
 int main()
 {
-  std::cout << "Hello world!\n";
+  Tree<int> t;
+  auto it = t.begin();
+  t.Insert(it, 1, -1);
+  t.Insert(it, 0, 1);
+  it.GoTo(0);
+  t.Insert(it, 1, -2);
+  t.Insert(it, 0, 2);
+  t.Print();
+
+
   return 0;
 }
