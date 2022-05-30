f5e8d9bed9f2c0e002c8d0fb57f37713bb951fd2
Лев Поляков <lev-polyackov@mail.ru>
2022-05-30 17:55:10 +0300

    in progress

---
+++ compiler/syntax.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <map>
+#include <vector>
+#include <iostream>
+
+enum class KeyWords
+{
+  _program = 1,
+  _const,
+  _var,
+  _begin,
+  _end,
+  _if,
+  _else
+};
+
+class Operations // Класс, хранящий поддерживаемые операции
+{
+private:
+  std::map<std::string, std::vector<int>> ops; // string - name of operation; vector<int> - priority, arity; ops - OPerationS
+public:
+  Operations() {// При добавлении операции в конструктор, обязательно нужно реализовать логику этой операции в Calc и добавить наименование в static string str_op()
+    ops.emplace("*", std::vector<int>{ 2, 2 });
+    ops.emplace("+", std::vector<int>{ 1, 2 });
+    ops.emplace("-", std::vector<int>{ 1, 2 });
+    ops.emplace("(", std::vector<int>{ 0, 0 });
+    ops.emplace(")", std::vector<int>{ 0, 0 });
+    ops.emplace("mod", std::vector<int>{ 3, 1 });
+    ops.emplace("div", std::vector<int>{ 3, 1 });
+  }
+
+  static std::string str_op() { return std::string("*, +, -, (, ), mod, div"); }
+
+  bool IsOperation(const std::string& elem) const { return ops.find(elem) != ops.end(); } // Проверка является ли данный элемент операцией, которая объявлена в классе
+
+  int GetPriority(const std::string& elem) { return ops[elem][0]; } // Получить приоритет у заданной операции, контроль использования за программистом 
+
+  int GetArity(const std::string& elem) { return ops[elem][1]; } // Получить арность у заданной операции, контроль использования за программистом 
+
+  int Calc(const std::string& elem, int left, int right = int()) {// Вычислить
+    if (elem == "*")
+      return left * right;
+    if (elem == "+")
+      return  left + right;
+    if (elem == "-")
+      return left - right;
+    if (elem == "mod")
+      return left % right;
+    if (elem == "div")
+      return left / right;
+  }
+};
+
+class SpecialWords
+{
+private:
+public:
+  SpecialWords()
+  {
+
+  }
+  void Execute(std::string operation, std::vector<std::string> arguments)
+  {
+
+  }
+};
+
--- compiler/variable.h
+++ compiler/variable.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <iostream>
+
+struct Variable
+{
+public:
+  union Var
+  {
+    double d;
+    int i;
+  };
+  Var v;
+  bool type; // false == int, true == double
+
+  bool operator==(const Variable& var) const;
+  bool operator!=(const Variable& var) const;
+  bool operator<(const Variable& var) const;
+  bool operator>(const Variable& var) const;
+  bool operator<=(const Variable& var) const;
+  bool operator>=(const Variable& var) const;
+  
+  Variable& operator=(const Variable& var);
+
+  Variable  operator+(const Variable& var);
+  Variable  operator-(const Variable& var);
+  Variable  operator*(const Variable& var);
+  Variable  operator%(const Variable& var);
+  Variable  operator/(const Variable& var);
+
+  friend std::istream& operator>>(std::istream& in, Variable& var);
+
+  friend std::ostream& operator<<(std::ostream& out, const Variable& var);
+};
